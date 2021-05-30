c466d05a41ae57d13d9c31985f3418346524e740
Fernando Chavez <fernandochajim@gmail.com>
2021-05-30 14:42:42 -0500

    actualizado numAleatorios

--- src/NumAleatorios.java
+++ src/NumAleatorios.java
@@ -1,20 +1,16 @@
-import java.util.Scanner;
 
 public class NumAleatorios {
-    int semillas[] = {4287, 9999, 4521, 1625, 6655, 2178};
+    int[] semillas = {4287, 9999, 4521, 1625, 6655, 2178};
     int cont = 0;
-    public  NumAleatorios(){
-    }
+
     public double numero(){
         //Las semillas deben ser siempre de 4 dígitos
-        double semillaCuad = Math.pow(semillas[cont],2);
+        double semillaCuad = semillas[cont] * semillas[cont];
         String preNum = Double.toString(semillaCuad);
         String num = preNum.substring(1,preNum.length());
+        //Número aleatorio genearado con semillas
         double numfinal = Double.parseDouble(num);
         cont++;
         return numfinal;
-    }
-    public void getSemillas(){
-
     }
 }
